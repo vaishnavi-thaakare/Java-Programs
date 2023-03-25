034c4bea15592b74686811c5abe8bd641102e849
Vaishnavi Thakare <93474040+vaishnavi-thaakare@users.noreply.github.com>
2023-03-25 14:36:09 +0530

    Output is added

--- src/JavaProgramsDemo/NumberpatternDemo6.java
+++ src/JavaProgramsDemo/NumberpatternDemo6.java
@@ -2,7 +2,7 @@ package JavaProgramsDemo;
 
 public class NumberpatternDemo6 {
 	public static void main (String[] args) {
-		System.out.println(" Number Pattern 6 ");
+		System.out.println(" Number Pattern  ");
 		for (int i=1;i<=4;i++) {
 			for (int j=i;j>=1;j--) {
 				System.out.print(j + " ");
@@ -12,3 +12,11 @@ public class NumberpatternDemo6 {
 	}
 
 }
+
+// Output
+ Number Pattern  
+1 
+2 1 
+3 2 1 
+4 3 2 1 
+
