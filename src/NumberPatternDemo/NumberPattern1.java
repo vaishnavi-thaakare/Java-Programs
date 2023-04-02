4f83db8e9913e8fd0e663c3b1913981c828cdc28
Vaishnavi Thakare <93474040+vaishnavi-thaakare@users.noreply.github.com>
2023-04-02 18:49:23 +0530

    Number Pattern Folder is added

---
+++ src/NumberPatternDemo/NumberPattern1.java
@@ -0,0 +1,21 @@
+package NumberPatternDemo;
+
+public class NumberPattern1 {
+	public static void main(String[] args) {
+		System.out.println(" Number Pattern");
+		for (int i=1;i<=5;i++) {
+			for (int j=1;j<=i;j++) {
+				System.out.print(i +" ");
+			
+			}
+			System.out.println();
+}}}
+
+/* Output
+ Number Pattern
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+*/
