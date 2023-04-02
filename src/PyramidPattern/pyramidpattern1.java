877fd78d5bed4d0ac0901736267291ff9b0cfc63
Vaishnavi Thakare <93474040+vaishnavi-thaakare@users.noreply.github.com>
2023-04-02 18:51:07 +0530

    pyramid pattern

---
+++ src/PyramidPattern/pyramidpattern1.java
@@ -0,0 +1,18 @@
+package PyramidPattern;
+
+public class pyramidpattern1 {
+		public static void main (String[] args) {
+			System.out.println(" Pattern");
+			for (int i=1;i<=4;i++) {
+				for ( int j=3;j>=i;j--) {
+					System.out.print(" ");
+				}
+				for (int j=1;j<=i;j++) {
+					System.out.print(" *");
+				}
+				System.out.println();
+			}
+		
+	}
+
+}
